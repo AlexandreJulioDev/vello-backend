cce172f66954dd20cf8b43d2608989753093797d
Alexandre Julio <Alexandre.julio8772@gamil.com>
2026-05-03 01:08:59 -0300

    fix: ajustar construtor do Prisma no seed para compatibilidade com v7

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,8 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const prisma = new PrismaClient();
+
 
 
 
